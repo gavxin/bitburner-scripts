10878eb963035a908b27c47d7060ecf7effbf8d4
gavxin <380274+gavxin@users.noreply.github.com>
2022-01-16 19:13:48 +0800

    Update

--- auto.js
+++ auto.js
@@ -3,6 +3,11 @@ export async function main(ns) {
   const files = ["weaken.js", "grow.js", "hack.js"];
   const scripts = ["weaken.js", "grow.js", "hack.js"];
 
+  function format_ram(r) {
+    let s = ns.nFormat(r * 1024 * 1024 * 1024, '0ib');
+    return s.substr(0, s.length - 2) + "B";
+  }
+
   function format_ms(t) {
     if (t < 1000) {
       return parseFloat(t).toFixed(0) + "ms";
@@ -162,11 +167,11 @@ export async function main(ns) {
     }
     result = result.reverse().slice(1);
     let s = "\n\n";
+    s += 'home;';
     for (let server of result) {
-      s += `connect ${server};\n`;
+      s += ` connect ${server};`;
     }
-    s += 'backdoor;\n';
-    s += 'home;\n';
+    s += ' backdoor;\n\n';
     ns.tprint(s);
   }
 
@@ -284,6 +289,14 @@ export async function main(ns) {
     }
   }
 
+  async function scp_back() {
+    for (let s of list_servers().filter(s => s.root && ns.getPurchasedServers().indexOf(s.host) == -1)) {
+      for (let f of ns.ls(s.host).filter(f => files.indexOf(f) == -1)) {
+        await ns.scp(f, s.host, "home");
+      }
+    }
+  }
+
   async function wait_for(condition) {
     while (true) {
       if (await condition()) return;
@@ -307,11 +320,15 @@ export async function main(ns) {
       return true;
     }
 
-    const sequences = [
+    let sequences = [
       {
         name: "starting ...",
         condition: async () => true,
-        action: async () => await nuke_and_exact_hack(),
+        action: async () => {
+          await nuke_all();
+          await super_hack_manual(agents_from("home"), "n00dles", 1, 1, 8);
+          return true;
+        },
       },
       {
         name: "money >= 200k$",
@@ -323,27 +340,31 @@ export async function main(ns) {
           return false;
         },
       },
-      {
-        name: "money >= 500k$",
-        condition: async () => ns.getPlayer().money >= 500 * 1000,
-        check: async () => ns.fileExists("BruteSSH.exe"),
-        action: async () => {
-          ns.tprint("\n\nPlease purchas BruteSSH.exe manually!\n");
-          ns.tprint("\n\nconnect darkweb\nbuy BruteSSH.exe\n");
-          return false;
-        },
-      },
-      {
-        name: "money >= 1.5m$",
-        condition: async () => ns.getPlayer().money >= 1.5 * 1000 * 1000,
-        check: async () => ns.fileExists("FTPCrack.exe"),
-        action: async () => {
-          ns.tprint("\n\nPlease purchas FTPCrack.exe manually!\n");
-          ns.tprint("\n\nconnect darkweb\nbuy FTPCrack.exe\n");
-          return false;
-        },
-      },
     ];
+    for (let program of programs) {
+      sequences.push({
+        name: `buy ${program.name}`,
+        condition: async () => ns.getPlayer().money >= program.price,
+        check: async () => ns.fileExists(program.name),
+        action: async () => {
+          ns.tprint("\n\nPlease purchas " + program.name + " manually!\n");
+          ns.tprint(`\n\nhome; connect darkweb; buy ${program.name}; \n`);
+          let buy_all = "\n\nhome; connect darkweb; ";
+          for (let p of programs) {
+            buy_all += "buy " + p.name + "; ";
+          }
+          ns.tprint(buy_all + "\n");
+          return false;
+        },
+      });
+    }
+
+    // sequences.push(
+    //   {
+    //     name: "buy "
+
+    //   }
+    // );
 
     for (let seq of sequences) {
       ns.tprint(`Running ${seq.name}:${localStorage.getItem(seq.name)} ...`);
@@ -377,7 +398,7 @@ export async function main(ns) {
     let s = "\n\n" + "price".padStart(6) + " | " + "raw".padStart(8) + " | " + "price\n";
     s += "-".repeat(32) + "\n";
     while (ns.getPurchasedServerCost(ram) < ns.getPlayer().money) {
-      s += ns.nFormat(ram * 1024 * 1024 * 1024, '0ib').padStart(6) + " | " + ram.toString().padStart(8) + " | " + ns.nFormat(ns.getPurchasedServerCost(ram), '0.00a$') + "\n";
+      s += format_ram(ram).padStart(6) + " | " + ram.toString().padStart(8) + " | " + ns.nFormat(ns.getPurchasedServerCost(ram), '0.00a$') + "\n";
       ram *= 2;
     }
     ns.tprint(s);
@@ -387,15 +408,15 @@ export async function main(ns) {
     let s = "\n\n" + "host".padEnd(12) + " | " + "ram".padStart(8) + "\n";
     s += "-".repeat(23) + "\n";
     ns.getPurchasedServers().forEach(n => {
-      s += n.padEnd(12) + " | " + ns.nFormat(ns.getServerMaxRam(n) * 1024 * 1024 * 1024, "0ib").padStart(8) + "\n";
+      s += n.padEnd(12) + " | " + format_ram(ns.getServerMaxRam(n)).padStart(8) + "\n";
     });
     ns.tprint(s);
   }
 
   async function b_buy(ram, name) {
-    name = name || "s" + ns.nFormat(ram * 1024 * 1024 * 1024, '0ib');
+    name = name || "s" + format_ram(ram);
     const n = ns.purchaseServer(name, ram);
-    ns.tprint(`buy complete, server name:${n}`);
+    ns.tprint(`✔ buy complete, server name:${n}`);
     if (n) {
       await ns.scp(files, "home", n);
     }
@@ -405,10 +426,10 @@ export async function main(ns) {
   function b_del(name) {
     ns.killall(name);
     if (!ns.deleteServer(name)) {
-      ns.tprint(`del server ${name} failed!`);
+      ns.tprint(`❌ del server ${name} failed!`);
       return false;
     }
-    ns.tprint(`del server ${name} success!`);
+    ns.tprint(`✔ del server ${name} success!`);
     return true;
   }
 
@@ -431,9 +452,9 @@ export async function main(ns) {
   }
 
   async function buy_super_weaken_grow_hack(target, ram = 1048576) {
-    let s = await b_buy(ram, `s${ns.nFormat(ram * 1024 * 1024 * 1024, "0ib")}_${target}`);
+    let s = await b_buy(ram, `s${format_ram(ram)}_${target}`);
     if (!s) {
-      ns.tprint(`Buy server failed! need money ${ns.nFormat(ns.getPurchasedServerCost(ram), '0.00a$')}, current have ${ns.getPurchasedServers().length}/25`);
+      ns.tprint(`❌ Buy server failed! need money ${ns.nFormat(ns.getPurchasedServerCost(ram), '0.00a$')}, current have ${ns.getPurchasedServers().length}/25`);
       return;
     }
     let agents = [{
@@ -441,7 +462,7 @@ export async function main(ns) {
       max_ram: ram,
     }];
     await super_weaken_grow_hack(agents, target);
-    ns.tprint(`bwgh target:${target} complete.`);
+    ns.tprint(`✔ bwgh target:${target} complete.`);
   }
 
   async function bwgh_all(count, ram, max_grow_min) {
@@ -449,20 +470,20 @@ export async function main(ns) {
     count = count || (25 - bought_servers.length);
     ram = ram || 1048576;
     max_grow_min = max_grow_min || 10;
-    ns.tprint(`count:${count}, ram:${ram}, max_grow_min:${max_grow_min}`);
+    // ns.tprint(`count:${count}, ram:${ram}, max_grow_min:${max_grow_min}`);
 
     let already_hacking = bought_servers.map(s => s.substr(s.indexOf('_') + 1));
-    ns.tprint("Already hacking:", already_hacking);
+    // ns.tprint("Already hacking:", already_hacking);
 
     let not_hacking = list_servers().filter(s => s.money_max > 0 && s.root && s.hackable && already_hacking.indexOf(s.host) == -1 && s.grow_ms < max_grow_min * 60 * 1000);
     not_hacking.sort((a, b) => b.money_max - a.money_max);
     not_hacking = not_hacking.map(s => s.host);
-    ns.tprint("Hackable servers:", not_hacking);
+    // ns.tprint("Hackable servers:", not_hacking);
 
     for (let i = 0; i < count; i++) {
       if (i >= not_hacking.length) break;
       if (ns.getPlayer().money < ns.getPurchasedServerCost(ram)) {
-        ns.tprint("No more money!");
+        ns.tprint("❌ No more money!");
         break;
       }
       let target = not_hacking[i];
@@ -470,7 +491,18 @@ export async function main(ns) {
       ns.exec("auto.js", "home", 1, "bwgh", target, ram);
       await ns.asleep(1000);
     }
-    ns.tprint(`bwgh all compelete!`);
+    ns.tprint(`✔ bwgh all compelete!`);
+  }
+
+  async function bwgh_best() {
+    let ram = 1024;
+    let min = 1;
+    while (ns.getPurchasedServerCost(ram * 2) < ns.getPlayer().money && ram * 2 <= 1024 * 1024) {
+      ram *= 2;
+      min += 1;
+    }
+    await bwgh_all(1, ram, min);
+    ns.tprint(`✔ bwgh best compelete! ram:${ram}, min:${min}`);
   }
 
   async function wgh_by_name() {
@@ -478,7 +510,7 @@ export async function main(ns) {
       let target = n.substr(n.indexOf("_") + 1);
       ns.tprint(`server ${n} target is ${target}, wgh it ...`);
       ns.exec("auto.js", "home", 1, "wgh", n, target);
-      await ns.asleep(1000);
+      await ns.asleep(2000);
     }
     return;
   }
@@ -489,8 +521,10 @@ export async function main(ns) {
   function agents_from(param) {
     if (param == "all") {
       return get_agents();
-    } else if (param == "nohome") {
+    } else if (param == "not_home") {
       return get_agents().filter(s => s.host != "home");
+    } else if (param == "not_mine") {
+      return get_agents().filter(s => s.host != "home").filter(s => ns.getPurchasedServers().indexOf(s.host) == -1);
     } else {
       try {
         let agents = [{
@@ -550,13 +584,13 @@ export async function main(ns) {
 
     const total_mem = agents.reduceRight((s, a) => s + a.max_ram, 0);
     const unit_mem = arr.reduceRight((s, c) => s + c.threads * c.ram * c.time_ratio, 0);
-    ns.tprint(`super_run w:${w}, g:${g}, h:${h}, unit count:${unit_count}, total_mem:${total_mem} unit mem:${unit_mem}`);
+    ns.tprint(`  super_run w:${w}, g:${g}, h:${h}, unit count:${unit_count}, total_mem:${total_mem} unit mem:${unit_mem}`);
 
     const kMinMs = 5;
     if (unit_count > arr[0].time / arr[0].time_ratio / kMinMs) {
       const old_unit_count = unit_count;
       unit_count = arr[0].time / arr[0].time_ratio / kMinMs;
-      ns.tprint(`unit count too large, you can't use FULL ram, change ${old_unit_count} to ${unit_count}`);
+      ns.tprint(`⚠ unit count too large, you can't use FULL ram, change ${old_unit_count} to ${unit_count}`);
     }
 
     agents.sort((a, b) => b.max_ram - a.max_ram);
@@ -567,7 +601,7 @@ export async function main(ns) {
     unit_count = Math.floor(unit_count);
 
     if (unit_count <= 0) {
-      ns.tprint(`unit count too small!`);
+      ns.tprint(`❌kkk unit count too small!`);
       return false;
     }
 
@@ -577,7 +611,7 @@ export async function main(ns) {
 
     const tick_time = Math.max(w_tick_time, g_tick_time, h_tick_time);
     const max_time_ratio = Math.max(arr[0].time_ratio, arr[1].time_ratio, arr[2].time_ratio);
-    ns.tprint(`w_tick_time:${w_tick_time}, g_tick_time:${g_tick_time}, h_tick_time:${h_tick_time}. tick_time:${tick_time}`);
+    ns.tprint(`  w_tick_time:${w_tick_time}, g_tick_time:${g_tick_time}, h_tick_time:${h_tick_time}. tick_time:${tick_time}`);
 
     let agent_index = 0;
     let agent_used_mem = 0;
@@ -597,7 +631,7 @@ export async function main(ns) {
         }
 
         if (agent_index >= agents.length) {
-          ns.tprint(`Something goes wrong when super_run ${target}!! already used all mems!! i:${i}, tick:${tick}, total:${arr[i].time_ratio * unit_count}`);
+          ns.tprint(`⚠ Something goes wrong when super_run ${target}!! already used all mems!! i:${i}, tick:${tick}, total:${arr[i].time_ratio * unit_count}`);
           return false;
         }
 
@@ -654,7 +688,6 @@ export async function main(ns) {
   }
 
   async function super_hack_analyze(agents, target) {
-
     let steal_percent = 40;
     while (steal_percent > 0) {
       const steal = ns.getServerMaxMoney(target) * steal_percent / 100;
@@ -677,43 +710,48 @@ export async function main(ns) {
   }
 
   async function super_hack(agents, target) {
-    let steal_percent = 40;
+    let steal_percent = 20;
     while (steal_percent > 0) {
       const steal = ns.getServerMaxMoney(target) * steal_percent / 100;
       const hack_threads = Math.floor(ns.hackAnalyzeThreads(target, steal));
-      const grow_threads = Math.ceil(ns.growthAnalyze(target, (100 / (100 - steal_percent * 2))));
+      const grow_threads = Math.ceil(ns.growthAnalyze(target, (100 / Math.max(100 - steal_percent * 2, 5))));
       const weaken_threads = Math.ceil((ns.hackAnalyzeSecurity(hack_threads) + ns.growthAnalyzeSecurity(grow_threads)) / ns.weakenAnalyze(1) * 1.2);
 
       const unit_count = get_super_run_unit_count(agents, target, weaken_threads, grow_threads, hack_threads);
-      ns.tprint(`w:${weaken_threads}, g:${grow_threads}, h:${hack_threads}, unit count:${unit_count}`);
+      ns.tprint(`    cheking super hack conditions... ${target} w:${weaken_threads}, g:${grow_threads}, h:${hack_threads}, unit count:${unit_count}`);
       if (unit_count <= 4) {
         steal_percent -= 1;
-        ns.tprint(`${target} unit count too small, steal percent to ${steal_percent}%`);
+        ns.tprint(`  ${target} unit count too small, steal percent to ${steal_percent}%`);
         continue;
       }
-      await super_run(agents, target, weaken_threads, grow_threads, hack_threads);
-      ns.tprint(`super hack ${target} started!`);
+      const kMaxUnitCount = 60;
+      if (unit_count > kMaxUnitCount && steal_percent < 90) {
+        steal_percent += 1;
+        continue;
+      }
+      await super_run(agents, target, weaken_threads, grow_threads, hack_threads, Math.floor(unit_count));
+      ns.tprint(`✔ super hack ${target} steal_percent:${steal_percent} started!`);
       return;
     }
-    ns.tprint(`super hack ${target} ERROR!`);
+    ns.tprint(`❌ super hack ${target} ERROR!`);
     return;
   }
 
   async function super_weaken_grow_hack(agents, target) {
     await super_weaken(agents, target);
     while (ns.getServerSecurityLevel(target) > ns.getServerMinSecurityLevel(target) * 1.1) {
-      ns.tprint(`Wait for target:${target} weaken...`);
+      ns.tprint(`= Wait for target:${target} weaken...`);
       await ns.asleep(30 * 1000);
       continue;
     }
     await super_grow(agents, target);
     while (ns.getServerMoneyAvailable(target) < ns.getServerMaxMoney(target) * 0.9 || ns.getServerSecurityLevel(target) > ns.getServerMinSecurityLevel(target) * 1.1) {
-      ns.tprint(`Wait for target:${target} grow...`);
+      ns.tprint(`= Wait for target:${target} grow...`);
       await ns.asleep(30 * 1000);
       continue;
     }
     await super_hack(agents, target);
-    ns.tprint(`wgh target:${target} complete.`);
+    ns.tprint(`✔ wgh target:${target} complete.`);
   }
 
   function usage() {
@@ -724,8 +762,10 @@ export async function main(ns) {
       "  list                         list servers, default depth is 5\n" +
       "  listf                        list servers and refresh every 1 seconds\n" +
       "  path <server>                path to server\n" +
+      "  path_all                     path to all server\n" +
       "  nuke                         nuke all available servers\n" +
       "  scp                          scp scripts to all available servers\n" +
+      "  scp_back                     scp servers content to home\n" +
       "  killall                      kill all server scripts\n" +
       "  startup [new]                startup from augmentation install\n" +
       "  b price                      buy server prices\n" +
@@ -768,8 +808,22 @@ export async function main(ns) {
     return await nuke_all();
   } else if (ns.args[0] == "scp") {
     return await scp_all();
+  } else if (ns.args[0] == "scp_back") {
+    return await scp_back();
   } else if (ns.args[0] == "path") {
     path_server(ns.args[1]);
+    return;
+  } else if (ns.args[0] == "path_all") {
+    const targets = [
+      "CSEC",
+      "avmnite-02h",
+      "I.I.I.I",
+      "run4theh111z",
+      "powerhouse-fitness",
+    ];
+    for (let t of targets) {
+      path_server(t);
+    }
     return;
   } else if (ns.args[0] == "killall") {
     list_servers().filter(s => s.root && s.max_ram > 0).forEach(s => ns.killall(s.host));
@@ -813,6 +867,7 @@ export async function main(ns) {
     let target = ns.args[2];
     return await super_grow(agents, target);
   } else if (ns.args[0] == "h") {
+    ns.tprint("ns.args:", ns.args);
     let agents = agents_from(ns.args[1]);
     if (agents.length == 0) {
       ns.tprint("Got no agents! Invalid argument!");
@@ -855,6 +910,9 @@ export async function main(ns) {
       await buy_super_weaken_grow_hack(target);
     }
     return;
+  } else if (ns.args[0] == "bwgh_best") {
+    await bwgh_best();
+    return;
   } else if (ns.args[0] == "bwgh_all") {
     let max_grow_min = ns.args[1];
     let ram = ns.args[2];
@@ -864,6 +922,7 @@ export async function main(ns) {
   } else if (ns.args[0] == "test") {
     return;
   } else {
+    ns.tprint("ns.args:", ns.args);
     return usage();
   }
 }
